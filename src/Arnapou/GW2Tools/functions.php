359be0c1222b226db207a46429d22873c38880f9
Arnaud Buathier <arnaud@arnapou.net>
2015-10-16 13:46:09 +0200

    better colored images

--- src/Arnapou/GW2Tools/functions.php
+++ src/Arnapou/GW2Tools/functions.php
@@ -189,7 +189,7 @@ function image($url) {
         return image($url->getSkin() ? $url->getSkin()->getIcon() : $url->getIcon());
     }
     elseif ($url instanceof Character || $url instanceof PvpStatsProfession || $url instanceof PvpGame) {
-        return '/assets/images/professions_wiki/' . $url->getProfession() . '.png';
+        return '/assets/images/professions_colored/' . $url->getProfession() . '.png';
     }
     elseif ($url instanceof CraftingDiscipline) {
         return '/assets/images/disciplines/' . $url->getName() . '.png';
